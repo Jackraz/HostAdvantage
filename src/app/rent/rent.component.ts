e9f40d1ccce742bbb8c59b1cf29e68249012d884
Jack Ramsden <j.ramsden@live.co.uk>
2021-02-20 00:41:42 +0000

    Converted all css to scss

--- src/app/rent/rent.component.ts
+++ src/app/rent/rent.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rent',
   templateUrl: './rent.component.html',
-  styleUrls: ['./rent.component.css']
+  styleUrls: ['./rent.component.scss']
 })
 export class RentComponent implements OnInit {
 
